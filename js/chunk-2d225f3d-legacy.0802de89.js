99415601eee473dae34469ee5e699c69b864c434
Onur Usluca <51825926+onurusluca@users.noreply.github.com>
2020-11-21 19:17:40 +0300

    Delete chunk-2d225f3d-legacy.0802de89.js

--- js/chunk-2d225f3d-legacy.0802de89.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d225f3d"],{e71f:function(c,n,w){"use strict";w.r(n);w("c6c5")}}]);
-//# sourceMappingURL=chunk-2d225f3d-legacy.0802de89.js.map
